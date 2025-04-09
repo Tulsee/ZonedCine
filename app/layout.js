a96c0f284a43b6cf17ed5e35548a100143e45c07
Tulsee <ramtulsi022@gmail.com>
2025-04-09 21:43:31 +0545

    style: Add global styles, color palette, and basic Navbar component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../components/Providers";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>
+          <Navbar />
           <main>{children}</main>
         </Providers>
       </body>
---
+++ components/Navbar.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchTerm.trim()) {
+      router.push(`/movies?search=${encodeURIComponent(searchTerm.trim())}`);
+      setSearchTerm("");
+    }
+  };
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navLeft}>
+        <Link href="/" className={styles.logo}>
+          Browse
+        </Link>
+      </div>
+      <div className={styles.navRight}>
+        <form onSubmit={handleSearch} className={styles.searchForm}>
+          <input
+            type="search"
+            placeholder="Search Movies..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className={styles.searchInput}
+          />
+          <button type="submit" className={styles.searchButton}>
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
